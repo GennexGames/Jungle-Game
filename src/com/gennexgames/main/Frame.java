5d3755e05bf78af6f37c6b95f117c1666a1fed12
Will Garrett <willgarrett610@gmail.com>
2015-04-05 00:26:36 -0600

    Create frame class

---
+++ src/com/gennexgames/main/Frame.java
@@ -0,0 +1,12 @@
+package com.gennexgames.main;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
